c91b4ad0d2c4abd73a7fb8176bb8a94e548ad6c1
fjebaker <fergusbkr@gmail.com>
2024-07-24 01:05:17 +0200

    feat: use more modular dynamic library approach

--- build.zig
+++ build.zig
@@ -1,10 +1,72 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+fn exists(b: *std.Build, path: []const u8) bool {
+    std.fs.cwd().access(b.pathFromRoot(path), .{ .mode = .read_only }) catch return false;
+    return true;
+}
+
+const flags = [_][]const u8{
+    "-fno-sanitize=undefined",
+};
+
+fn build_language(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, lang: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(b.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const basedir = try std.fmt.allocPrint(alloc, "tree-sitter-{s}", .{lang});
+    const srcdir = try std.fs.path.join(alloc, &.{ basedir, "src" });
+    const querydir = try std.fs.path.join(alloc, &.{ basedir, "queries" });
+
+    const query_file_name = try std.fmt.allocPrint(alloc, "{s}-highlights.scm", .{basedir});
+    const query_file = try std.fs.path.join(alloc, &.{ querydir, "highlights.scm" });
+    const parser = try std.fs.path.join(alloc, &.{ srcdir, "parser.c" });
+    const scanner = try std.fs.path.join(alloc, &.{ srcdir, "scanner.c" });
+    const scanner_cc = try std.fs.path.join(alloc, &.{ srcdir, "scanner.cc" });
+
+    const lib = b.addSharedLibrary(.{
+        .name = basedir,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.addCSourceFiles(.{ .files = &.{parser}, .flags = &flags });
+    lib.linkLibC();
+
+    if (exists(b, scanner)) {
+        lib.addCSourceFiles(.{ .files = &.{scanner}, .flags = &flags });
+    }
+
+    if (exists(b, scanner_cc)) {
+        lib.addCSourceFiles(.{ .files = &.{scanner_cc}, .flags = &flags });
+    }
+
+    const install_file = b.addInstallFile(b.path(query_file), query_file_name);
+    install_file.dir = .lib;
+
+    const install_lib = b.addInstallArtifact(lib, .{});
+    install_file.step.dependOn(&install_lib.step);
+    b.getInstallStep().dependOn(&install_file.step);
+}
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const syntax_dep = b.dependency("syntax", .{ .target = target, .optimize = optimize });
+    if (b.option([]const u8, "lang", "Build a language extension.")) |lang| {
+        try build_language(b, target, optimize, lang);
+        return;
+    }
+
+    const treez_dep = b.dependency(
+        "treez",
+        .{ .target = target, .optimize = optimize },
+    );
+    const treez = treez_dep.module("treez");
+    const tree_sitter_dep = treez_dep.builder.dependency(
+        "tree-sitter",
+        .{ .target = target, .optimize = optimize },
+    );
 
     const exe = b.addExecutable(.{
         .name = "zig-syntax-test",
@@ -12,7 +74,10 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    exe.root_module.addImport("syntax", syntax_dep.module("syntax"));
+    exe.root_module.addImport("treez", treez);
+
+    exe.linkLibC();
+    exe.linkLibrary(tree_sitter_dep.artifact("tree-sitter"));
 
     b.installArtifact(exe);
 
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,115 @@
 const std = @import("std");
-const syntax = @import("syntax");
+const treez = @import("treez");
+
+const LanguageFn = *const fn () callconv(.C) ?*const treez.Language;
+
+const LanguageSpec = struct {
+    allocator: std.mem.Allocator,
+    dylib: std.DynLib,
+    lang: *const treez.Language,
+    highlights: []const u8,
+
+    pub fn deinit(self: *LanguageSpec) void {
+        self.allocator.free(self.highlights);
+        self.dylib.close();
+    }
+};
+
+fn load_language(allocator: std.mem.Allocator, language: []const u8) !LanguageSpec {
+    const lib_path = try std.fmt.allocPrint(
+        allocator,
+        "zig-out/lib/libtree-sitter-{s}.so",
+        .{language},
+    );
+    defer allocator.free(lib_path);
+    const hl_path = try std.fmt.allocPrint(
+        allocator,
+        "zig-out/lib/tree-sitter-{s}-highlights.scm",
+        .{language},
+    );
+    defer allocator.free(hl_path);
+    const func_name = try std.fmt.allocPrintZ(
+        allocator,
+        "tree_sitter_{s}",
+        .{language},
+    );
+    defer allocator.free(func_name);
+
+    var lib = try std.DynLib.open(lib_path);
+    errdefer lib.close();
+
+    const scm = try std.fs.cwd().readFileAlloc(allocator, hl_path, try std.math.powi(usize, 2, 32));
+    errdefer allocator.free(scm);
+
+    return .{
+        .allocator = allocator,
+        .dylib = lib,
+        .lang = (lib.lookup(LanguageFn, func_name) orelse unreachable)().?,
+        .highlights = scm,
+    };
+}
+
+fn CallBack(comptime T: type) type {
+    return fn (
+        ctx: T,
+        sel: treez.Range,
+        scope: []const u8,
+        id: u32,
+        capture_idx: usize,
+    ) anyerror!void;
+}
+
+const SyntaxHighlighter = struct {
+    allocator: std.mem.Allocator,
+    lang_spec: LanguageSpec,
+    parser: *treez.Parser,
+    query: *treez.Query,
+    tree: ?*treez.Tree = null,
+
+    fn parse(self: *SyntaxHighlighter, content: []const u8) !void {
+        if (self.tree) |tree| tree.destroy();
+        self.tree = try self.parser.parseString(null, content);
+    }
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        content: []const u8,
+        language: []const u8,
+    ) !SyntaxHighlighter {
+        const lang_spec = try load_language(allocator, language);
+        var self: SyntaxHighlighter = .{
+            .allocator = allocator,
+            .lang_spec = lang_spec,
+            .parser = try treez.Parser.create(),
+            .query = try treez.Query.create(lang_spec.lang, lang_spec.highlights),
+        };
+        errdefer self.deinit();
+        try self.parser.setLanguage(lang_spec.lang);
+        try self.parse(content);
+        return self;
+    }
+
+    pub fn deinit(self: *SyntaxHighlighter) void {
+        if (self.tree) |t| t.destroy();
+        self.query.destroy();
+        self.parser.destroy();
+        self.lang_spec.deinit();
+    }
+
+    pub fn walk(self: *const SyntaxHighlighter, ctx: anytype, comptime cb: CallBack(@TypeOf(ctx))) !void {
+        const cursor = try treez.Query.Cursor.create();
+        defer cursor.destroy();
+        const tree = if (self.tree) |p| p else return;
+        cursor.execute(self.query, tree.getRootNode());
+        while (cursor.nextMatch()) |match| {
+            var idx: usize = 0;
+            for (match.captures()) |capture| {
+                try cb(ctx, capture.node.getRange(), self.query.getCaptureNameForId(capture.id), capture.id, idx);
+                idx += 1;
+            }
+        }
+    }
+};
 
 const Ctx = struct {
     content: []const u8,
@@ -7,11 +117,10 @@ const Ctx = struct {
 
     fn call_back(
         self: *Ctx,
-        range: syntax.Range,
+        range: treez.Range,
         scope: []const u8,
         id: u32,
         idx: usize,
-        _: *const syntax.Node,
     ) !void {
         _ = id;
         if (idx > 0) return;
@@ -46,11 +155,11 @@ pub fn main() !void {
     );
     defer allocator.free(content);
 
-    const parser = try syntax.create_guess_file_type(allocator, content, args[1]);
-    defer parser.destroy();
+    var parser = try SyntaxHighlighter.init(allocator, content, "zig");
+    defer parser.deinit();
 
     var ctx: Ctx = .{
         .content = content,
     };
-    try parser.render(&ctx, Ctx.call_back, null);
+    try parser.walk(&ctx, Ctx.call_back);
 }
